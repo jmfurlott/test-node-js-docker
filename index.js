b92c5f8238b61066c3b2bc0f1b4e3f38521e26b4
Joseph Furlott <jmfurlott@gmail.com>
2014-10-30 10:31:55 -0400

    added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/myapp');
 
 var app = express();
+
 
 app.get('/', function(req, res){
     res.send('Hello world');
